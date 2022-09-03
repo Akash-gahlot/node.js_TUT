0fbfac47014e21b8129343a3da42f019b2ebf7dc
Akash_gahlot <akashgahlot1926@gmail.com>
2022-09-03 19:25:31 +0530

    update and delete in mongoDB

--- updateMongo.js
+++ updateMongo.js
@@ -3,14 +3,15 @@ import getinfo from "./connectToDB.js";
 
 const update = async () => { 
     let collection = await getinfo();
-    return collection.deleteOne({ "name": "Vaibhav" });
+    //console.log(collection);
+   // return collection.insertOne({name:"Meera",Department:"Teaching",Office:"Meerut",Package:"6"});
+    // let update = await collection.updateOne({ Name: "Meera" }, { $set: { Name: "Ayush", Office: "Gurugram" } });
+    // return update;
+    let del =await collection.deleteOne({ Name: "Ayush" });
+    return del;
+
 }
 
 const result = await update();
 
-if (result.acknowledged) {
-    console.log("Data updated successully");
-}
-else { 
-    console.log("Data not updated");
-}
+console.warn(result);
